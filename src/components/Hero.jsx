c003f73fdf7a989c500ed206e4aa3b7f0d0aa19e
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-29 16:47:42 -0500

    Finish Tutorial
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Vignesh</span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            High School Senior<br className='sm:block hidden' />
-            Aspiring Software Engineer
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
           </p>
         </div>
       </div>
