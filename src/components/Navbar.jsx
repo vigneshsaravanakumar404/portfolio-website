15eae3dc20cfeef23839fcd12b945c8b2fc26383
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-29 14:44:17 -0500

    Fix vite + tailwindcss + postcss issue
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,7 @@ const Navbar = () => {
 
         >
           <img src = {logo} alt = "logo" className = "w-9 h-9 object-contain" />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Vignesh <span className='sm:block hidden'> Saravanakumar</span></p>
         </Link>
       </div>
     </nav>
