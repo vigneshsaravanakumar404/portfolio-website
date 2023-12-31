5e44c9f177ef01beac00dcd173018619af9bb8c0
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-30 22:54:09 -0500

    Co-authored-by: Roshan Sujith <justsomeon123@users.noreply.github.com>
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/404.jsx
+++ src/404.jsx
@@ -2,23 +2,40 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { styles } from "./styles"
 import { Navbar } from "./components";
+//TODO: replace navbar so it actually works
 
-const App = () => {
+const NotFound = () => {
+  return (
+      <section className={`relative w-full h-screen mx-auto`}>
+          <div
+              className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+          >
+              <p className={`${styles.heroHeadText} mt-2 text-cyan-600`}> 404 </p>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}> oopsie you made a whoopsie </p>
+              
+          </div>
+      </section>
+  );
+};
+
+
+const Fourofour = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
+          <NotFound />
         </div>
       </div>
-    </BrowserRouter>  
+    </BrowserRouter>
   );
-}
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Fourofour />
   </React.StrictMode>
 );
-
