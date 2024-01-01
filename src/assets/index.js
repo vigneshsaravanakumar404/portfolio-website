6fd36c988331d9b47c328962d2e1c45533d2e6e1
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-01-01 18:58:49 -0500

    Custom Scrollbar
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,15 +5,9 @@ import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
+
 import close from "./close.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
   logo,
@@ -23,11 +17,5 @@ export {
   web,
   github,
   menu,
-  close,
-  meta,
-  shopify,
-  starbucks,
-  carrent,
-  jobit,
-  tripguide,
+  close
 };
